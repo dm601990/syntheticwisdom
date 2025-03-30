4681eaa419d76d034cdf40de9e687f542865504e
Your Name <user@example.com>
2025-03-30 21:43:02 +0200

    Add Vercel Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
+import { Analytics } from '@vercel/analytics/react';
 import './globals.css';
 
 const inter = Inter({ subsets: ['latin'] });
@@ -26,6 +27,7 @@ export default function RootLayout({
       </head>
       <body className={inter.className}>
         <div id="app-container">{children}</div>
+        <Analytics />
       </body>
     </html>
   );
